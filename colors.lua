886cb64beeb75f0cd4abb53f52e46f870122e8aa
edsaibert <eduardasaibert@ufpr.br>
2024-04-23 18:52:29 -0300

    adição do mesh, porém com erros na renderização

--- colors.lua
+++ colors.lua
@@ -2,17 +2,20 @@
 
 local colors = {}
 
-colors.indigo = { red = 45/255, green = 26/255, blue = 82/255, alpha = 255}
-colors.purple = { red = 79/255, green = 23/255, blue = 86/255, alpha = 255}
-colors.light_purple = { red = 97/255, green = 21/255, blue = 89/255, alpha = 255}
-colors.pink = { red = 152/255, green = 16/255, blue = 98/255, alpha = 255}
-colors.off_white = { red = 250/255, green = 249/255, blue = 246/255, alpha = 255}
-colors.light_blue = { red = 149/255, green = 222/255, blue = 255/255, alpha = 255}
-colors.blue = { red = 24/255, green = 171/255, blue = 237/255, alpha = 255}
+colors.indigo = { red = 45/255, green = 26/255, blue = 82/255, alpha = 1}
+colors.purple = { red = 79/255, green = 23/255, blue = 86/255, alpha = 1}
+colors.light_purple = { red = 97/255, green = 21/255, blue = 89/255, alpha = 1}
+colors.pink = { red = 152/255, green = 16/255, blue = 98/255, alpha = 1}
+colors.off_white = { red = 250/255, green = 249/255, blue = 246/255, alpha = 1}
+colors.light_blue = { red = 149/255, green = 222/255, blue = 255/255, alpha = 1}
+colors.blue = { red = 24/255, green = 171/255, blue = 237/255, alpha = 1}
 
 function colors:setColor(color)
     love.graphics.setColor(color.red, color.green, color.blue, color.alpha)
 end
 
+function colors:returnColorObject(color)
+    return {color.red, color.green, color.blue, color.alpha}
+end
 
 return colors
