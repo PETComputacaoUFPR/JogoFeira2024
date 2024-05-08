20d2942e73f59873d68887876215839672c429ee
RobertoTomchak <robertotomchak@gmail.com>
2024-05-08 10:12:48 -0300

    Add white color

--- colors.lua
+++ colors.lua
@@ -9,6 +9,7 @@ colors.pink = { red = 152/255, green = 16/255, blue = 98/255, alpha = 1}
 colors.off_white = { red = 250/255, green = 249/255, blue = 246/255, alpha = 1}
 colors.light_blue = { red = 149/255, green = 222/255, blue = 255/255, alpha = 1}
 colors.blue = { red = 24/255, green = 171/255, blue = 237/255, alpha = 1}
+colors.white = { red = 255/255, green = 255/255, blue = 255/255, alpha = 1}
 
 function colors:setColor(color)
     love.graphics.setColor(color.red, color.green, color.blue, color.alpha)
